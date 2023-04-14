275199a8848f450d8b6ec540890903de23de2597
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-15 08:37:26 +0900

    チャット欄を適当に

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+import type { Message } from './interfaces/Message';
+
+const dummy: Message[] = [
+  { speaker: "user", text: "USERです" },
+  { speaker: "bot", text: "BOTです" },
+]
 </script>
 
 <template lang="pug">
@@ -6,13 +12,28 @@
   header
     h1 ChatGPT
   main
-    textarea.prompt(placeholder="プロンプトを入力してください")
+    .inputArea
+      textarea.prompt(placeholder="プロンプトを入力してください")
+    .chatArea
+      template(v-for=("(item, index) in dummy") :key="index")
+        .message(:class="{ 'user': item.speaker === 'user', 'bot': item.speaker === 'bot' }") {{ item.text }}
 </template>
 
 <style lang="sass" scoped>
 .root
-  .prompt
-    resize: none
-    width: 350px
-    height: 50px
+  .inputArea
+    .prompt
+      resize: none
+      width: 350px
+      height: 50px
+  .chatArea
+    width: 300px
+    .message
+      
+      &.user
+        text-align: left
+        background-color: #0f0
+      &.bot
+        text-align: right
+        background-color: #F22
 </style>
