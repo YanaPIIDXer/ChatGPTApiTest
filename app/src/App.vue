1c39f797db8d7ef186b23872fcd2afabcaea7aac
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-15 08:25:27 +0900

    とりあえずプロジェクト作成

---
+++ app/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template lang="pug">
+.root
+  header
+    h1 ChatGPT
+  main
+    textarea.prompt(placeholder="プロンプトを入力してください")
+</template>
+
+<style lang="sass" scoped>
+.root
+  .prompt
+    resize: none
+    width: 350px
+    height: 50px
+</style>
