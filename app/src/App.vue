b90beb996782d251377a2addcc97a0310e3dd889
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-15 09:09:14 +0900

    ChatGPTにプロンプトを投げて、レスポンスを表示する処理を実装

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,35 @@
 <script setup lang="ts">
 import { ref, type Ref } from "vue";
 import type { Message } from './interfaces/Message';
+import { Configuration, OpenAIApi } from "openai";
 
 const messages: Ref<Message[]> = ref([]);
 const inputPrompt = ref("");
+
+const configuration = new Configuration({
+  apiKey: import.meta.env.VITE_CHATGPT_TOKEN,
+});
+delete configuration.baseOptions.headers['User-Agent'];
+const openai = new OpenAIApi(configuration);
 
 /**
  * 送信
  */
 const onSend = async () => {
-  messages.value.push({ speaker: "user", text: inputPrompt.value });
-  // TODO: 送信処理実装
+  const text = inputPrompt.value;
   inputPrompt.value = "";
+  messages.value.push({ speaker: "user", text });
+
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo-0301",
+      messages: [{ role: "user", content: text }],
+    });
+    messages.value.push({ speaker: "bot", text: response.data.choices[0].message?.content ?? "Invalid content." })
+  } catch (error) {
+    alert("エラー");
+    console.error(error);
+  }
 }
 </script>
 
